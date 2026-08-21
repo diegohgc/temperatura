babfabb366dd4d3b291ed8a0398831cd3ca6203b
diegohgc <diegohg72@gmail.com>
2026-08-21 13:58:13 +0200

    Fix modal hoy: usar weatherCode validado con observaciones, no AEMET crudo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v13';
+const CACHE = 'temperatura-v14';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
