ea6387e7652b5bc053d00c568197461c05aa2731
diegohgc <diegohg72@gmail.com>
2026-08-21 10:59:42 +0200

    Bump SW cache a v8 para forzar actualizacion en todos los usuarios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v7';
+const CACHE = 'temperatura-v8';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
