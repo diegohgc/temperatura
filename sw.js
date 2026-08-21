ba4ab5fd7fb1c8280885addd72f4fa1e3bf6177a
diegohgc <diegohg72@gmail.com>
2026-08-21 13:10:05 +0200

    Fix prob lluvia: usar Open-Meteo horario en vez de bloque 6h de AEMET

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v8';
+const CACHE = 'temperatura-v9';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
