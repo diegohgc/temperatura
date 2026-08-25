62d5fdd0f95213bd5740ff64799861dbbed9b789
diegohgc <diegohg72@gmail.com>
2026-08-25 21:46:11 +0200

    Arco solar: puntos calculados manualmente, arco perfecto + guia tenue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v23';
+const CACHE = 'temperatura-v24';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
