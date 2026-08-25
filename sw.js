2aecef4a9740fc5c80d3f7caeab38bf10101ab18
diegohgc <diegohg72@gmail.com>
2026-08-25 21:24:02 +0200

    Iconos de clima en color con gradientes SVG

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v20';
+const CACHE = 'temperatura-v21';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
