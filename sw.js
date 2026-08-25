1c55665d15b5b86f25db61c465f55e043e61c78e
diegohgc <diegohg72@gmail.com>
2026-08-25 20:43:23 +0200

    Arco solar real en panel sol: declinacion solar + latitud + posicion actual

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v14';
+const CACHE = 'temperatura-v15';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
