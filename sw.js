19a13cff349f362e550b4e1716eeb833fe6b6cdb
diegohgc <diegohg72@gmail.com>
2026-08-25 21:37:05 +0200

    Arco solar: hora local ciudad + rediseno con sol rayos y glow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v21';
+const CACHE = 'temperatura-v22';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
