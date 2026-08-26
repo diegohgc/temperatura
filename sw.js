57ce89b84e0727bc1a79b81f8d7f0328f8d60b5c
diegohgc <diegohg72@gmail.com>
2026-08-26 08:11:24 +0200

    Modal dia: icono siempre directo sin ajuste de probabilidad

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v29';
+const CACHE = 'temperatura-v30';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
