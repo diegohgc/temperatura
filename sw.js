4346287a3ced3576baf226fd3af97c974ff508e3
diegohgc <diegohg72@gmail.com>
2026-08-26 10:30:53 +0200

    Redeseno cajas velocidad/distancia en modal brujula

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v31';
+const CACHE = 'temperatura-v32';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
