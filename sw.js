9ecf823bb12e32c74074464e1868ddddd02d888e
diegohgc <diegohg72@gmail.com>
2026-08-26 11:00:58 +0200

    OWM 800-804: usar para nubosidad actual sobre modelos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v35';
+const CACHE = 'temperatura-v36';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
