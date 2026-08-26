98b97a6ef284bfd2e93717292aea1939b37cca70
diegohgc <diegohg72@gmail.com>
2026-08-26 10:46:31 +0200

    Fix: cloudCover solo nubes bajas+medias, no cirros

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v33';
+const CACHE = 'temperatura-v34';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
