940e422e0b1f20e8e68bf359723d69d320a688f7
diegohgc <diegohg72@gmail.com>
2026-08-27 23:13:14 +0200

    WeatherAPI: prediccion horaria con iconos y temperatura reales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v37';
+const CACHE = 'temperatura-v38';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
