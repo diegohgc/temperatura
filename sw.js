760309f906e3ff59f0680057ebeb41f596c2f399
diegohgc <diegohg72@gmail.com>
2026-08-27 20:20:51 +0200

    Integrar WeatherAPI como fuente principal del cielo actual

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v36';
+const CACHE = 'temperatura-v37';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
