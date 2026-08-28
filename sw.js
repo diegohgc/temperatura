6d8fc151e08202b48415d6191f1ab19c377c0054
diegohgc <diegohg72@gmail.com>
2026-08-28 16:34:16 +0200

    Quitar colores de numeros UV y calidad del aire

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v49';
+const CACHE = 'temperatura-v50';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
