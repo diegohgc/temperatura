ae8e891baad9105cb3ab623b5297cfd595061a37
diegohgc <diegohg72@gmail.com>
2026-08-28 15:12:38 +0200

    Color semaforo en calidad del aire segun AQI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v43';
+const CACHE = 'temperatura-v44';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
