723e9a81f632d60dc00f0d94597c368d92c1aa8b
diegohgc <diegohg72@gmail.com>
2026-08-28 09:40:15 +0200

    Probabilidad lluvia proxima hora desde WeatherAPI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v38';
+const CACHE = 'temperatura-v39';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
