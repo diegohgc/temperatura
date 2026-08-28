ea853c2f34619fbaa0e5020afe7c6f63395f08b7
diegohgc <diegohg72@gmail.com>
2026-08-28 15:24:06 +0200

    Color calidad del aire en fondo de tarjeta con borde sutil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v45';
+const CACHE = 'temperatura-v46';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
