38678bd84f03d517cfe4020c666a83c876eedbc6
diegohgc <diegohg72@gmail.com>
2026-08-28 15:18:13 +0200

    UV, Sol restante y Calidad del aire como tarjetas independientes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v44';
+const CACHE = 'temperatura-v45';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
