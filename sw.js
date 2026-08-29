b539fd89ab696cfa5f1b9d44592759b659b573a3
diegohgc <diegohg72@gmail.com>
2026-08-29 23:49:32 +0200

    Icono y texto 100% WeatherAPI puro, sin correcciones de AEMET ni OM

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v52';
+const CACHE = 'temperatura-v53';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
