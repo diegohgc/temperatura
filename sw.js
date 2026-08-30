af6faf5d37bd5fe5497036b3dc71d9e9f082fc02
diegohgc <diegohg72@gmail.com>
2026-08-30 20:44:51 +0200

    Igualar tamaño de valores de tarjetas 2x2 al de UV (1.6rem)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v61';
+const CACHE = 'temperatura-v62';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
