11bf79cd586247c04642f415a16c0362d80eb89d
diegohgc <diegohg72@gmail.com>
2026-08-30 20:58:04 +0200

    Tarjeta viento: layout columna (VIENTO / 5 km/h / del Noroeste)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v64';
+const CACHE = 'temperatura-v65';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
