00fc9b23d6c54952c906c3ac8d1e8a60aeddb4d1
diegohgc <diegohg72@gmail.com>
2026-08-30 23:21:52 +0200

    Auto-ajuste font-size interior-valor: encoge hasta caber en tarjeta

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v81';
+const CACHE = 'temperatura-v82';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
