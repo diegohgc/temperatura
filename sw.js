211fffe7c49e459bb705091154960e3c6fdb9382
diegohgc <diegohg72@gmail.com>
2026-08-30 20:16:25 +0200

    Tarjetas 2x2 para lluvia/sensacion/viento/humedad, modal viento con brujula y rafagas, aire muestra nivel en texto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v55';
+const CACHE = 'temperatura-v56';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
