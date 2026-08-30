8acaf00ce522baabacd988d9bce85037c201bad3
diegohgc <diegohg72@gmail.com>
2026-08-30 23:02:37 +0200

    SW v79

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v78';
+const CACHE = 'temperatura-v79';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
