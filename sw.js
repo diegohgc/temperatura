43cbac7e70925d93acb658310d4b83369bcb736b
diegohgc <diegohg72@gmail.com>
2026-08-30 11:06:06 +0200

    Subir umbral de lluvia real de 0.1mm a 0.2mm

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v53';
+const CACHE = 'temperatura-v54';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
