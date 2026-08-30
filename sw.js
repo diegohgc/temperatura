4352e2db1318a7c78dafa41b394510d058b1d9a5
diegohgc <diegohg72@gmail.com>
2026-08-30 22:17:45 +0200

    Modal lluvia: prob actual, acumulado hoy, proxima lluvia, grafico 24h (19 idiomas)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v72';
+const CACHE = 'temperatura-v73';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
