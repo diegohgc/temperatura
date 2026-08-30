7e5e0a73918b61deec342fc0894d04c35c8bbf22
diegohgc <diegohg72@gmail.com>
2026-08-30 22:36:45 +0200

    Fix modal lluvia: eliminar referencia a grafHoras que ya no existe en el HTML

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v76';
+const CACHE = 'temperatura-v77';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
