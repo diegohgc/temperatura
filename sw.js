5fbaa19d2686d6a1687924eb0610472cc1b89316
diegohgc <diegohg72@gmail.com>
2026-08-30 23:18:04 +0200

    Fix tarjeta: UV label corto, nowrap interior-label, textoCorto grupos sensibles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v80';
+const CACHE = 'temperatura-v81';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
