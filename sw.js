741511c6ea952088606f2353872b6df9227553fd
diegohgc <diegohg72@gmail.com>
2026-08-30 20:51:43 +0200

    Tarjeta viento: quitar flecha, valor a 1.6rem, direccion en texto completo multiidioma

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v63';
+const CACHE = 'temperatura-v64';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
