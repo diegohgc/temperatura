f1a2a8c9c2a74a36c09ba4d00eab709fcb6f517a
diegohgc <diegohg72@gmail.com>
2026-08-30 20:36:59 +0200

    Etiqueta Calidad del aire en dos lineas para que no desborde

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v59';
+const CACHE = 'temperatura-v60';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
