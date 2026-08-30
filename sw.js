6833c6ec08d5d560fa6760108517109c18791911
diegohgc <diegohg72@gmail.com>
2026-08-30 22:23:25 +0200

    Fix modal lluvia: wapiData como global _wapiDataGlobal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v73';
+const CACHE = 'temperatura-v74';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
