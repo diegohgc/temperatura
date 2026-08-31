7c454bc0635658d11ce776122059e91a5e0c95fa
diegohgc <diegohg72@gmail.com>
2026-08-31 17:26:20 +0200

    Wind direction: label 'Procedencia' + full text (del Norte) in modal stat

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v85';
+const CACHE = 'temperatura-v86';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
