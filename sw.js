5f3de47dfc3910efaea2b68032c8fc6734a47ef9
diegohgc <diegohg72@gmail.com>
2026-08-31 19:43:02 +0200

    Modal dia: viento actual de WeatherAPI para hoy, max para otros dias

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v94';
+const CACHE = 'temperatura-v95';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
