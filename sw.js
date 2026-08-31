88f67b6ddd1198648ff41ff2525f4c5b1dd1b780
diegohgc <diegohg72@gmail.com>
2026-08-31 18:58:31 +0200

    Show settings panel while loading location on startup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v89';
+const CACHE = 'temperatura-v90';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
