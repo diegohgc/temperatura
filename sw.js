c489ed712de9ce76c12ee2967d95364df51c87a8
diegohgc <diegohg72@gmail.com>
2026-09-02 20:13:59 +0200

    Corregir mezcla de idiomas en modales para pt-BR y otras variantes regionales

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v96';
+const CACHE = 'temperatura-v97';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
