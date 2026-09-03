532b12ee7c052b364b51f5bad76014720d9fde83
diegohgc <diegohg72@gmail.com>
2026-09-03 10:25:03 +0200

    Distinguir bruma (1030) de niebla (1135) y niebla helada (1147) en todos los idiomas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'temperatura-v104';
+const CACHE = 'temperatura-v105';
 const ASSETS = ['./manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
